351ea42cf09c8e95869fcc9455a184770be190d8
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2025-11-08 14:20:29 -0300

    first commit

---
+++ aula-junit/src/test/java/bankui2/aula_junit/AulaJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package bankui2.aula_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
